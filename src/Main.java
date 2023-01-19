19d110562534321c791f76e463c8aa1fabd7110f
Olga <deputat.d@yandex.com>
2023-01-19 13:58:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //стоимость билета 15000//
+        int price = 15000;
+        //стоимость одной 1 бонусной мили//
+        int bonus = 20;
+
+        System.out.println((price / bonus) + " Бонус программы: Колличество бесплатных миль за перелет");
+    }
+}
